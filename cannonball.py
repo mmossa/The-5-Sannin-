7c33b719d053b3ad404d7699651873dd709a5402
rahmed23 <90023035+rahmed23@users.noreply.github.com>
2021-12-20 17:06:03 -0500

    Add files via upload

---
+++ cannonball.py
@@ -0,0 +1,29 @@
+""" Simulate a game where the user shoots cannonballs at the moving ship. """
+
+import pygame
+from pygame.sprite import Sprite 
+
+class CannonBall(Sprite):
+    """Manages the cannonballs shot at the ship object.
+    
+    Attributes: 
+		game: the settings for the game
+    """
+    
+    def __init__(self, game):
+		super().__init__()
+		self.screen = game.screen
+		self.settings = game.settings
+
+		# create the rect for the cannonball
+		self.color = self.settings.cannonball_color	
+		self.rect = pygame.Rect(0,0, self.settings.cannonball_width, self.settings.cannonball_height)
+		self.rect.midtop = game.ship.rect.midtop
+ 	
+	def update(self):
+		# update cannonball rect y postion by cannonball speed
+		self.rect.y -= self.settings.cannonball_speed
+
+	def draw_cannonball(self):
+		# draw the cannonball rect onto the screen
+	    pygame.draw.rect(self.screen, self.color, self.rect)
